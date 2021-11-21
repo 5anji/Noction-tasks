566243152bd82b53997dce964fb7f442711c91e2
Gaby <60102030+5anji@users.noreply.github.com>
2021-11-21 22:43:16 +0200

    Completed Exercises 1-3

--- src/task_1/exercises.cc
+++ src/task_1/exercises.cc
@@ -3,20 +3,21 @@
 float a, b;
 
 void ex_1(float x, float y, float z) {
-  a = 0;
-  b = 0;
+  a = (sqrtf(fabs(x - 1)) - cbrtf(fabsf(y))) /
+      (1 + (powf(x, 2) / 2) + (powf(y, 2) / 4));
+  b = x * (atanf(z) + powf(M_E, -(x + 3)));
   std::cout << "A = " << a << std::endl;
   std::cout << "B = " << b << std::endl;
 }
 void ex_2(float x, float y, float z) {
-  a = 0;
-  b = 0;
+  a = (3 + powf(M_E, y - 1)) / (1 + powf(x, 2) * fabsf(y - tanf(z)));
+  b = 1 + fabsf(y - x) + ((powf(y - x, 2)) / 2) + ((powf(fabsf(y - x), 3)) / 3);
   std::cout << "A = " << a << std::endl;
   std::cout << "B = " << b << std::endl;
 }
 void ex_3(float x, float y, float z) {
-  a = 0;
-  b = 0;
+  a = (1 + y) * ((x + y)) / (powf(M_E, -x - 2)); // (x^2 + 4) can be simplified
+  b = ((1 + cosf(y - 2)) * (2 + powf(sinf(z), 2))) / (powf(x, 4));
   std::cout << "A = " << a << std::endl;
   std::cout << "B = " << b << std::endl;
 }
