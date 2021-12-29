dd131bfa426985b52169cb83d427fbe2c9c074d5
Gîtlan Gabriel <ggitlan@noction.com>
2021-12-29 12:15:41 +0200

    Created RBTree algorithm

---
+++ src/task_5/main.cpp
@@ -0,0 +1,19 @@
+// Implementing Red-Black Tree in C++
+
+#include "tree.h"
+
+int main() {
+  RedBlackTree bst;
+  bst.insert(55);
+  bst.insert(40);
+  bst.insert(65);
+  bst.insert(60);
+  bst.insert(75);
+  bst.insert(57);
+
+  bst.printTree();
+  std::cout << std::endl
+            << "After deleting" << std::endl;
+  bst.deleteNode(40);
+  bst.printTree();
+}
