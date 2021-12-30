b1341c8253189e4a9a0c4362af79d4aac9dda5a2
Gaby <60102030+5anji@users.noreply.github.com>
2021-12-30 23:57:02 +0200

    Re-formated text (task_2)

--- src/task_2/other.cpp
+++ src/task_2/other.cpp
@@ -16,14 +16,16 @@ void display_menu(std::string* a) {
     std::cout << Bold(Blue(" 1")) << Orange(" - Display the list") << std::endl
               << Bold(Blue(" 2")) << Orange(" - Add node at the end") << std::endl
               << Bold(Blue(" 3")) << Orange(" - Add node at the start") << std::endl
-              << Bold(Blue(" 4")) << Orange(" - Add node at the ") + Bold(Orange("n")) + Orange(" position")
-              << std::endl
+              << Bold(Blue(" 4")) << Orange(" - Add node at the ") + Bold(Orange("n")) + Orange(" position") << std::endl
               << Bold(Blue(" 5")) << Orange(" - Delete node from the end") << std::endl
               << Bold(Blue(" 6")) << Orange(" - Delete node from the start") << std::endl
-              << Bold(Blue(" 7")) << Orange(" - Delete node from the ") + Bold(Orange("n")) + Orange(" position")
-              << std::endl
-              << Bold(Blue(" 8")) << Orange(" - Change data from the ") + Bold(Orange("n")) + Orange(" position")
-              << std::endl
+              << Bold(Blue(" 7")) << Orange(" - Delete node from the ") + Bold(Orange("n")) + Orange(" position") << std::endl
+              << Bold(Blue(" 8")) << Orange(" - Change data from the ") + Bold(Orange("n")) + Orange(" position") << std::endl
+              << Bold(Blue(" 4")) << Orange(" - Add node at the ") + Bold(Orange("n")) + Orange(" position") << std::endl
+              << Bold(Blue(" 5")) << Orange(" - Delete node from the end") << std::endl
+              << Bold(Blue(" 6")) << Orange(" - Delete node from the start") << std::endl
+              << Bold(Blue(" 7")) << Orange(" - Delete node from the ") + Bold(Orange("n")) + Orange(" position") << std::endl
+              << Bold(Blue(" 8")) << Orange(" - Change data from the ") + Bold(Orange("n")) + Orange(" position") << std::endl
               << Bold(Blue(" 9")) << Orange(" - Copy this list to the second") << std::endl
               << Bold(Blue("10")) << Orange(" - Display the next list") << std::endl
               << Bold(Blue(" d")) << Orange(" - Fully delete the list") << std::endl
