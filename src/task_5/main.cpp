575845a7bc47fca334d03fe7a292041422972987
Gîtlan Gabriel <ggitlan@noction.com>
2021-12-30 16:01:44 +0200

    reformated files using clang-format

--- src/task_5/main.cpp
+++ src/task_5/main.cpp
@@ -1,27 +1,18 @@
-// Implementing Red-Black Tree in C++
-
 #include "tree.h"
 // #include <random>
 
 int main() {
-  RedBlackTree bst;
-  // std::random_device generator;
-  // std::uniform_int_distribution<int> distribution(1, 100);
+    RedBlackTree bst;
+    // std::random_device generator;
+    // std::uniform_int_distribution<int> distribution(1, 100);
 
-  for (size_t i = 0; i < 25; i++) {
-    bst.insert(i+1);
-  }
-  
+    for (size_t i = 0; i < 25; i++) bst.insert(i + 1);
 
-  bst.printTree();
+    bst.printTree();
 
-  for (size_t i = 3; i < 6; i++)
-  {
-    bst.deleteNode(i+1);
-  }
-  
-  // std::cout << std::endl
-  //           << "After deleting" << std::endl;
-  // bst.deleteNode(40);
-  bst.printTree();
+    for (size_t i = 3; i < 6; i++) bst.deleteNode(i + 1);
+
+    std::cout << std::endl << "After deleting" << std::endl;
+    // bst.deleteNode(40);
+    bst.printTree();
 }
--- src/task_5/tree.h
+++ src/task_5/tree.h
@@ -1,46 +1,47 @@
-#include <iostream>
 #include "colors.h"
+
+#include <iostream>
 struct Node;
 
 // typedef Node *NodePtr;
 
 class RedBlackTree {
 private:
-  Node* root;
-  Node* TNULL;
+    Node* root;
+    Node* TNULL;
 
-  void initializeNULLNode(Node* node, Node* parent);
-  void preOrderHelper(Node* node);
-  void inOrderHelper(Node* node);
-  void postOrderHelper(Node* node);
+    void initializeNULLNode(Node* node, Node* parent);
+    void preOrderHelper(Node* node);
+    void inOrderHelper(Node* node);
+    void postOrderHelper(Node* node);
 
-  Node* searchTreeHelper(Node* node, int key);
+    Node* searchTreeHelper(Node* node, int key);
 
-  void deleteFix(Node* x);
-  void rbTransplant(Node* u, Node* v);
-  void deleteNodeHelper(Node* node, int key);
-  void insertFix(Node* k);
-  void printHelper(Node* root, std::string indent, bool last);
+    void deleteFix(Node* x);
+    void rbTransplant(Node* u, Node* v);
+    void deleteNodeHelper(Node* node, int key);
+    void insertFix(Node* k);
+    void printHelper(Node* root, std::string indent, bool last);
 
 public:
-  RedBlackTree();
+    RedBlackTree();
 
-  void preorder();
-  void inorder();
-  void postorder();
+    void preorder();
+    void inorder();
+    void postorder();
 
-  Node* searchTree(int k);
-  Node* minimum(Node* node);
-  Node* maximum(Node* node);
-  Node* successor(Node* x);
-  Node* predecessor(Node* x);
+    Node* searchTree(int k);
+    Node* minimum(Node* node);
+    Node* maximum(Node* node);
+    Node* successor(Node* x);
+    Node* predecessor(Node* x);
 
-  void leftRotate(Node* x);
-  void rightRotate(Node* x);
-  void insert(int key);
+    void leftRotate(Node* x);
+    void rightRotate(Node* x);
+    void insert(int key);
 
-  Node* getRoot();
+    Node* getRoot();
 
-  void deleteNode(int data);
-  void printTree();
+    void deleteNode(int data);
+    void printTree();
 };
