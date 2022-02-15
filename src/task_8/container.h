4c27755aef14f9d48335ce62b3b3b3d401eec467
5anji <gitlanm@gmail.com>
2022-02-15 12:44:37 +0200

    implementing virtual inheritance

--- src/task_8/container.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-template <typename T>
-struct Container {
-    virtual void Print_Raw() {}
-};
--- src/task_8/main.cpp
+++ src/task_8/main.cpp
@@ -1,33 +1,4 @@
-// #include "mylibs/list.h"
-// #include "mylibs/tree.h"
-
-// #include <vector>
-
-// int main() {
-//     std::vector<Container<int>*> vec;
-//     RedBlackTree<int> tree1, tree2;
-//     Linked_List<int> list1, list2;
-
-//     tree1.insert(1);
-//     list1.Add_Node_End(2);
-
-//     vec.push_back(&tree1);
-//     vec.push_back(&list1);
-//     vec.push_back(&list2);
-//     vec.push_back(&tree2);
-
-//     list2.Add_Node_End(3);
-//     tree2.insert(4);
-
-//     for (size_t i = 0; i < vec.size(); i++) {
-//         vec[i]->Print_Raw();
-//     }
-
-//     return 0;
-// }
-
 #include <iostream>
-#include <vector>
 
 struct Animals {
     virtual void say() {
@@ -47,32 +18,26 @@ struct Dog : virtual public Animals {
     }
 };
 
-struct Mammals : public Cat, public Dog {
+struct Mammals : public Cat
+        , public Dog {
     void say() {
         std::cout << "Mu" << std::endl;
     }
 };
 
-
 int main() {
-    std::vector<Animals*> vec;
     Dog d;
     Cat c;
     Mammals m;
-    Animals* a1, *a2, *a3;
+    Animals* a[3];
 
-    a1 = &d;
-    a2 = reinterpret_cast<Animals*>(&c);
-    a3 = &m;
+    a[0] = &d;
+    a[1] = reinterpret_cast<Animals*>(&c);
+    a[2] = &m;
 
-    vec.push_back(a1);
-    vec.push_back(a2);
-    vec.push_back(a3);
-    
-    for (size_t i = 0; i < vec.size(); i++) {
-        vec[i]->say();
+    for (size_t i = 0; i < 3; i++) {
+        a[i]->say();
     }
-
 
     return 0;
 }
